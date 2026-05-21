748aa7d4862893e627539925b9425a738c082673
nicher92 <niclas.hertzberg@gmail.com>
2026-05-21 15:42:19 +0300

    cleanup

--- launch_all_swarms.sh
+++ launch_all_swarms.sh
@@ -3,4 +3,4 @@
 # Submits an array of 70 jobs. 
 # This tells Slurm to run the script 70 times, setting $SLURM_ARRAY_TASK_ID 
 # to a value from 0 to 69 for each respective run.
-sbatch --array=0-2 scripts/train-0.05B-ne_test.sh
+sbatch --array=0-2 scripts/train-0.05B.sh
